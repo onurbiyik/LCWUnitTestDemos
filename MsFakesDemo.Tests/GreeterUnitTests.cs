5514bdbdfa741c9c0565abe806a8e4bbc4ec1636
onurbiyik <onurbiyik@gmail.com>
2022-12-05 13:04:44 +0300

    Add project files.

---
+++ MsFakesDemo.Tests/GreeterUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.QualityTools.Testing.Fakes;
+
+namespace MsFakesDemo.Tests
+{
+    public class GreeterUnitTests
+    {
+        [Theory]
+        [InlineData(02, 00, "İyi geceler")]
+        [InlineData(09, 00, "Günaydın")]
+        [InlineData(11, 00, "İyi günler")]
+        [InlineData(12, 15, "İyi günler")]
+        [InlineData(19, 00, "İyi akşamlar")]
+        [InlineData(23, 59, "İyi geceler")]
+        public void TestGreeter(int hours, int minutes, string expected)
+        {
+            using (ShimsContext.Create())
+            {
+                // Arrange
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2022, 11, 05, hours, minutes, 00);
+
+                var sut = new Greeter();
+
+                // Act
+                var result = sut.Greet();
+
+                // Assert
+                result.Should().Be(expected);
+
+            }
+        }
+    }
+}
---
+++ MsFakesDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Greeter
+{
+    public string Greet()
+    {
+        var timeOfDay = DateTime.Now;
+
+        return timeOfDay.Hour switch
+        {
+            > 04 and < 11 => "Günaydın",
+            > 11 and < 17 => "İyi günler",
+            > 17 and < 21 => "İyi akşamlar",
+            _ => "İyi geceler"
+        };
+    }
+}
